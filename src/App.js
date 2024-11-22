79dd5c99d7c8e2c7d057ae0b60902e14a1566c68
YadamVinay369 <vinay.y21@iiits.in>
2024-11-22 21:59:41 +0530

    added basic routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import logo from "./logo.svg";
 import "./App.css";
-
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home.jsx";
+import About from "./pages/about/About.jsx";
+import Contact from "./pages/contact/Contact.jsx";
+import Resume from "./pages/resume/Resume.jsx";
+import Portfolio from "./pages/portfolio/Portfolio.jsx";
+import Skills from "./pages/skills/Skills.jsx";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
