09de461784995ee07a875d9963271746c1188c9c
YadamVinay369 <vinay.y21@iiits.in>
2024-11-23 13:14:24 +0530

    feat: react-tsparticles integrated into the website successfully

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import particles from "./utils/particles.js";
 import Home from "./pages/home/Home.jsx";
 import About from "./pages/about/About.jsx";
 import Contact from "./pages/contact/Contact.jsx";
 import Resume from "./pages/resume/Resume.jsx";
 import Portfolio from "./pages/portfolio/Portfolio.jsx";
 import Skills from "./pages/skills/Skills.jsx";
+import Navbar from "./components/navbar/navbar.jsx";
 function App() {
+  const handleInit = async (main) => {
+    await loadFull(main);
+  };
   return (
     <div className="App">
+      <Particles id="tsparticles" options={particles} init={handleInit} />
+
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
