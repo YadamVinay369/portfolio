b07346ffdb5072f2b2187ea17ba4fd8c344a6f3b
YadamVinay369 <vinay.y21@iiits.in>
2024-11-24 14:19:04 +0530

    chore: conditional rendering done for particles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
 import particles from "./utils/particles.js";
@@ -11,13 +11,17 @@ import Portfolio from "./pages/portfolio/Portfolio.jsx";
 import Skills from "./pages/skills/Skills.jsx";
 import Navbar from "./components/navbar/navbar.jsx";
 function App() {
+  const location = useLocation();
+
+  const renderLoc = location.pathname === "/";
   const handleInit = async (main) => {
     await loadFull(main);
   };
   return (
     <div className="App">
-      <Particles id="tsparticles" options={particles} init={handleInit} />
-
+      {renderLoc && (
+        <Particles id="tsparticles" options={particles} init={handleInit} />
+      )}
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
