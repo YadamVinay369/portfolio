8f20b236e6ca8403989b80af39615e7629b9ebc3
YadamVinay369 <vinay.y21@iiits.in>
2024-11-26 14:19:22 +0530

    feat: skills section created successfully

---
+++ src/pages/skills/utils.js
@@ -0,0 +1,59 @@
+const skillsData = [
+  {
+    label: "Front End",
+    data: [
+      {
+        skillName: "HTML",
+        percentage: "90",
+      },
+      {
+        skillName: "CSS",
+        percentage: "60",
+      },
+      {
+        skillName: "JS",
+        percentage: "80",
+      },
+      {
+        skillName: "ReactJS",
+        percentage: "70",
+      },
+    ],
+  },
+  {
+    label: "Back End",
+    data: [
+      {
+        skillName: "NodeJS",
+        percentage: "80",
+      },
+      {
+        skillName: "ExpressJS",
+        percentage: "60",
+      },
+      {
+        skillName: "MongoDB",
+        percentage: "60",
+      },
+    ],
+  },
+  {
+    label: "CS Fundamentals",
+    data: [
+      {
+        skillName: "SQL",
+        percentage: "90",
+      },
+      {
+        skillName: "OS",
+        percentage: "70",
+      },
+      {
+        skillName: "OOPS",
+        percentage: "80",
+      },
+    ],
+  },
+];
+
+export default skillsData;
