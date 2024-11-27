482980c8e6a0d1ad5ac5ea37092dc217634fe3ab
YadamVinay369 <vinay.y21@iiits.in>
2024-11-28 02:37:18 +0530

    resume section completed

--- src/pages/resume/Resume.jsx
+++ src/pages/resume/Resume.jsx
@@ -1,5 +1,13 @@
 import { BsInfoCircleFill } from "react-icons/bs";
 import PageHeader from "../../components/pageHeader/PageHeader";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import resumeData from "./utils";
+import "./Resume.scss";
+import { MdWork } from "react-icons/md";
 
 const Resume = () => {
   return (
@@ -8,6 +16,72 @@ const Resume = () => {
         headerText="my resume"
         icon={<BsInfoCircleFill size={40} />}
       />
+
+      <div className="timeline">
+        <div className="timeline__experience">
+          <h3 className="timeline__experience__header-text">Experience</h3>
+          <VerticalTimeline
+            layout={"1-column"}
+            lineColor="var(--yellow-theme-main-color)"
+          >
+            {resumeData.experience.map((item, i) => (
+              <VerticalTimelineElement
+                key={i}
+                className="timeline__experience__vertical-timeline-element"
+                contentStyle={{
+                  background: "none",
+                  color: "var(--yellow-theme-sub-text-color)",
+                  border: "1.5px solid var(--yellow-theme-main-color)",
+                }}
+                date={item.timeline}
+                icon={<MdWork />}
+                iconStyle={{
+                  background: "#181818",
+                  color: "var(--yellow-theme-main-color)",
+                }}
+              >
+                <div className="vertical-timeline-element-title-wrapper">
+                  <h3>{item.title}</h3>
+                  <h4>{item.subTitle}</h4>
+                </div>
+                <p>{item.description}</p>
+              </VerticalTimelineElement>
+            ))}
+          </VerticalTimeline>
+        </div>
+
+        <div className="timeline__education">
+          <h3 className="timeline__education__header-text">Education</h3>
+          <VerticalTimeline
+            layout={"1-column"}
+            lineColor="var(--yellow-theme-main-color)"
+          >
+            {resumeData.education.map((item, i) => (
+              <VerticalTimelineElement
+                key={i}
+                className="timeline__education__vertical-timeline-element"
+                contentStyle={{
+                  background: "none",
+                  color: "var(--yellow-theme-sub-text-color)",
+                  border: "1.5px solid var(--yellow-theme-main-color)",
+                }}
+                date={item.timeline}
+                icon={<MdWork />}
+                iconStyle={{
+                  background: "#181818",
+                  color: "var(--yellow-theme-main-color)",
+                }}
+              >
+                <div className="vertical-timeline-element-title-wrapper">
+                  <h3>{item.title}</h3>
+                  <h4>{item.subTitle}</h4>
+                </div>
+                <p>{item.description}</p>
+              </VerticalTimelineElement>
+            ))}
+          </VerticalTimeline>
+        </div>
+      </div>
     </section>
   );
 };
---
+++ src/pages/resume/utils.jsx
@@ -0,0 +1,42 @@
+const resumedata = {
+  experience: [
+    {
+      title: "SDE Intern",
+      timeline: "Aug 2024 - Aug 2024",
+      subTitle: "Hyderabad (Work from home)",
+      description:
+        "Created a user-authentication system and Role-Based-Access-Control System using NodeJs,MongoDB",
+    },
+    {
+      title: "Alpha Tester Intern",
+      timeline: "Aug 2024 - Sept 2024",
+      subTitle: "kerala (Work from home)",
+      description:
+        "Performed multiple tests on Yoliday application and found nearly 15 bugs.",
+    },
+  ],
+  education: [
+    {
+      title: "Btech Computer Science",
+      timeline: "April 2021 - April 2025",
+      subTitle: "IIIT Sricity",
+      description: "Currently, I am a final year student.",
+    },
+    {
+      title: "Intermediate (11th and 12th)",
+      timeline: "March 2018 - March 2020",
+      subTitle: "Sri Chaitanya College, Vizag",
+      description:
+        "Studied Maths, Physics and Chemistry. Especially, I love Mathematics.",
+    },
+    {
+      title: "Schooling upto 10th Standard",
+      timeline: "Aug 2024 - Aug 2024",
+      subTitle: "Gayatri School, Srikakulam",
+      description:
+        "Completed my schooling with 9.8 GPA. Ramanujan Math olympiad winner in my 9th standard.",
+    },
+  ],
+};
+
+export default resumedata;
