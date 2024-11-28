87c766cf2e13a816500f41445c1ce6d44a53cef0
YadamVinay369 <vinay.y21@iiits.in>
2024-11-28 19:53:34 +0530

    feat: projects section created successfully

--- src/pages/portfolio/Portfolio.jsx
+++ src/pages/portfolio/Portfolio.jsx
@@ -1,13 +1,136 @@
+import { useState } from "react";
 import { BsInfoCircleFill } from "react-icons/bs";
 import PageHeader from "../../components/pageHeader/PageHeader";
+import BreakUp from "../../images/BreakUp.png";
+import DonorHub from "../../images/DonorHub.png";
+import ImageTracer from "../../images/ImageTracer.png";
+import InsightRise from "../../images/InsightRise.png";
+import Marime from "../../images/Marime.png";
+import MermoryGame from "../../images/MemoryGame.png";
+import Praanayam from "../../images/Praanayam.png";
+import "./Portfolio.scss";
+
+const portfolioData = [
+  {
+    id: 2,
+    name: "DonorHub",
+    image: DonorHub,
+    link: "https://github.com/YadamVinay369/DonorHub",
+  },
+  {
+    id: 2,
+    name: "ImageTracer",
+    image: ImageTracer,
+    link: "https://github.com/YadamVinay369/DonorHub",
+  },
+  {
+    id: 2,
+    name: "Marime",
+    image: Marime,
+    link: "https://github.com/YadamVinay369/DonorHub",
+  },
+  {
+    id: 2,
+    name: "InsightRise",
+    image: InsightRise,
+    link: "https://github.com/YadamVinay369/DonorHub",
+  },
+  {
+    id: 3,
+    name: "BreakUp",
+    image: BreakUp,
+    link: "https://github.com/YadamVinay369/DonorHub",
+  },
+  {
+    id: 3,
+    name: "MemoryGame",
+    image: MermoryGame,
+    link: "https://github.com/YadamVinay369/DonorHub",
+  },
+  {
+    id: 3,
+    name: "Praanayam",
+    image: Praanayam,
+    link: "https://github.com/YadamVinay369/DonorHub",
+  },
+];
+
+const filteredData = [
+  {
+    filterId: 1,
+    label: "All",
+  },
+  {
+    filterId: 2,
+    label: "Development",
+  },
+  {
+    filterId: 3,
+    label: "Games",
+  },
+];
 
 const Portfolio = () => {
+  const [filteredValue, setfilteredValue] = useState(1);
+  const [hoveredValue, setHoveredValue] = useState(null);
+
+  const handleFilter = (currentId) => {
+    setfilteredValue(currentId);
+  };
+
+  const handleHover = (index) => {
+    setHoveredValue(index);
+  };
+
+  const filteredItems =
+    filteredValue === 1
+      ? portfolioData
+      : portfolioData.filter((item) => item.id === filteredValue);
+
   return (
     <section id="portfolio" className="portfolio">
       <PageHeader
         headerText="My Portfolio"
         icon={<BsInfoCircleFill size={40} />}
       />
+      <div className="portfolio__content">
+        <ul className="portfolio__content__filter">
+          {filteredData.map((item) => (
+            <li
+              className={item.filterId === filteredValue ? "active" : ""}
+              onClick={() => handleFilter(item.filterId)}
+              key={item.filterId}
+            >
+              {item.label}
+            </li>
+          ))}
+        </ul>
+
+        <div className="portfolio__content__cards">
+          {filteredItems.map((item, index) => (
+            <div
+              className="portfolio__content__cards__item"
+              key={`cardItem${item.name.trim()}`}
+              onMouseEnter={() => handleHover(index)}
+              onMouseLeave={() => handleHover(null)}
+            >
+              <div className="portfolio__content__cards__item__image-wrapper">
+                <a>
+                  <img src={item.image} alt="dummy data" />
+                </a>
+              </div>
+              <div className="overlay">
+                {index === hoveredValue && (
+                  <div>
+                    <p>{item.name}</p>
+                    <button>Github</button>
+                  </div>
+                )}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
